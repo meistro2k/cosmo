b58e6764cb8b2b5ba53cf683f439252f8c1123c5
Peter Tran <1421122+meistro2k@users.noreply.github.com>
2025-03-27 12:18:45 +1100

    update routercmd

--- router/cmd/custom/main.go
+++ router/cmd/custom/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	routercmd "github.com/wundergraph/cosmo/router/cmd"
+	routercmd "github.com/meistro2k/cosmo/router/cmd"
 	// Import your modules here
 	_ "github.com/meistro2k/cosmo/router/cmd/custom/module"
 )
